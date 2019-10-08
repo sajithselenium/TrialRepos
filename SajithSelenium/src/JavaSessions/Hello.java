6e01b90d8e96352beedf0128f8d2df3fcef9101a
sajith <sajith@Lenovo-PC.Home>
2019-10-08 06:29:21 +0100

    Initial version of project

---
+++ SajithSelenium/src/JavaSessions/Hello.java
@@ -0,0 +1,11 @@
+package JavaSessions;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//This is first program created
+		System.out.println("This is a first program written by me");
+		
+	
+	}
+}
